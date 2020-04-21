11ed9450c37fb5727d212def5a72d862aff1ce7d
sensui <sensuijunior@hotmail.com>
2020-04-21 19:09:08 -0300

    range and progress bar

---
+++ src/app/pages/progress-bar/progress-bar.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bar',
+  templateUrl: './progress-bar.page.html',
+  styleUrls: ['./progress-bar.page.scss'],
+})
+export class ProgressBarPage implements OnInit {
+
+  porcentaje = 0.05;
+  finalValue;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  cambioRango(event) {
+    console.log(event);
+    this.porcentaje = event.detail.value / 100;
+    this.finalValue = event.detail.value;
+  }
+
+}
