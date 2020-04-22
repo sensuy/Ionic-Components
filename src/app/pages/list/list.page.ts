148d8b4f590b7946e62dccd702cc84de0d9ff765
sensui <sensuijunior@hotmail.com>
2020-04-22 06:12:19 -0300

    toast

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { IonItemSliding } from '@ionic/angular';
+import { IonItemSliding, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -16,7 +16,8 @@ export class ListPage implements OnInit {
   usuarios: Observable<any>;
 
   constructor(
-    private dataService: DataService
+    private dataService: DataService,
+    private toastCtrl: ToastController
   ) { }
 
   ngOnInit() {
@@ -25,16 +26,28 @@ export class ListPage implements OnInit {
     );
   }
 
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
   favorite(user) {
     console.log('favorite: ', user);
     this.lista.closeOpened();
+    this.presentToast('Gurdó en favoritos');
   }
   share(user) {
     console.log('share: ', user);
+    this.presentToast('Compartido');
     this.lista.closeOpened();
   }
   borrar(user) {
     console.log('borrar: ', user);
+    this.presentToast('Borrado');
     this.lista.closeOpened();
   }
 }
